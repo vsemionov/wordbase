fc54300b0d306fd020fe2419bd123a8df0ff35e2
Victor Semionov <vsemionov@gmail.com>
2011-09-16 19:46:29 +0300

    Delete dicts.

---
+++ src/tools/del_pgsql.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2011 Victor Semionov
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#  * Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#  * Neither the name of the copyright holder nor the names of the contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+import sys
+import os
+
+import psycopg2
+
+import pgutil
+
+
+delete_dictionary = "DELETE FROM {}.dictionaries WHERE name = %s;"
+
+
+script_name = os.path.basename(__file__)
+
+
+def usage():
+    print("Usage: {} [-f conf_file] dict [...]".format(script_name), file=sys.stderr)
+    print("Removed wordbase pgsql dictionary.", file=sys.stderr)
+
+def del_pgsql_task(cur, schema, dict_names):
+    for name in dict_names:
+        cur.execute(delete_dictionary.format(schema), (name, ))
+
+
+dict_names = pgutil.get_pgsql_params(None, -1, usage)
+
+if not len(dict_names):
+    usage(2)
+    sys.exit(2)
+
+pgutil.process_pgsql_task(del_pgsql_task, dict_names)
