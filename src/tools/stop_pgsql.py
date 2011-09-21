3fe96c19dc4c893ba98533dce50ce8561682bd87
Victor Semionov <vsemionov@gmail.com>
2011-09-21 20:47:15 +0300

    Stop marker.

---
+++ src/tools/stop_pgsql.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2011 Victor Semionov
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#  * Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#  * Neither the virt_name of the copyright holder nor the names of the contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+import sys
+import os
+
+import psycopg2
+
+import pgutil
+
+
+STOP_DB_NAME = "--exit--"
+
+STOP_DB_DESCRIPTION = "Stop default search here."
+
+script_name = os.path.basename(__file__)
+
+
+def usage():
+    print("Usage: {} [-f conf_file] db_order", file=sys.stderr)
+    print("Adds a search-stopping meta-dictionary marker in pgsql.", file=sys.stderr)
+
+options, (db_order, ) = pgutil.get_pgsql_params("o:i:", 1, 1, usage)
+
+db_order = int(db_order)
+
+pgutil.process_pgsql_task(pgutil.import_task, db_order, STOP_DB_NAME, STOP_DB_DESCRIPTION, None, [], True)
